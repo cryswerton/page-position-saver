2e97e76d8b63c8f4829afe2ba7a769c59ab8f69e
cryswerton <cryswertons@gmail.com>
2021-09-25 20:10:18 -0300

    saving the link and the scroll position of the html page, if the link is already stored it just updates the scroll position of the actual link

--- background.js
+++ background.js
@@ -1,3 +1,20 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, {file: "content.js"});
  });
+
+   
+//function called once the extension is installed
+chrome.runtime.onInstalled.addListener( function(){
+    let myObjArray = []
+      chrome.storage.sync.get(['key'], function(result){
+        if(result['key']){
+          if(result['key'].length > 0){
+            myObjArray = result['key']
+          }
+        }
+        chrome.storage.sync.set({key: myObjArray}, function(){
+          console.log('Database successfully initialized.')
+          console.log('Amount of keywords loaded: ' + myObjArray.length)
+        })
+      })
+      })
--- content.js
+++ content.js
@@ -1,3 +1,60 @@
-var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
-console.log(scrollPos)
-document.getElementsByTagName("html")[0].scrollTop = '9116';
+function containsObject(obj, array) {
+    var i;
+    for (i = 0; i < array.length; i++) {
+        if (array[i].link === obj.link) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function updateChromeStorage(key, obj) {
+	var myObjArray = [];
+ 
+	chrome.storage.sync.get(key, function(result) {
+		if (result) { 
+			myObjArray = result[key];
+		}
+ 
+
+        if(!containsObject(obj, myObjArray)){
+            console.log('It does not exist.')
+            myObjArray.push(obj)
+            chrome.storage.sync.set({key: myObjArray}, function() {
+                console.log(`New object added: ${obj.link}`)
+            });
+        }else{
+            console.log('It already exists, updating the scroll position...')
+            obj.scrollP = document.getElementsByTagName("html")[0].scrollTop
+            myObjArray.push(obj)
+            chrome.storage.sync.set({key: myObjArray}, function() {
+                console.log(`Scroll position updated: ${obj.scrollP}`)
+            });
+             
+        }
+	});
+ }
+
+{
+     let link = window.location.href
+     let scrollP = document.getElementsByTagName("html")[0].scrollTop
+     let obj = {link: link, scrollP: scrollP}
+     updateChromeStorage('key', obj)
+
+     chrome.storage.sync.get(['key'], function(result){
+        if(result['key'].length > 0){
+            let myObjArray = result['key']
+            console.log(`Database length: ${myObjArray.length}`)
+        }
+     })
+ }
+
+
+
+
+
+
+
+
+
