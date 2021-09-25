02f7e03862bd3a4f4caeb7888b38c1fc5ce30612
cryswerton <cryswertons@gmail.com>
2021-09-25 13:30:29 -0300

    just one position saved for just one page, lol

---
+++ content.js
@@ -0,0 +1,3 @@
+var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
+console.log(scrollPos)
+document.getElementsByTagName("html")[0].scrollTop = '9116';
